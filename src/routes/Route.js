d258466789540a62e9ed1e6050d28d8d4fc19ed7
Riviana Zenker <rivianazenker@MacBook-Air-de-Riviana.local>
2020-02-19 21:31:45 -0300

    added default Layouts

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
 export default function RouterWrapper({
   component: Component,
   isPrivate,
@@ -14,8 +17,19 @@ export default function RouterWrapper({
   if (signed && !isPrivate) {
     return <Redirect to="/dashboard" />;
   }
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Route {...rest} component={Component} />;
+
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 RouterWrapper.propTypes = {
   isPrivate: PropTypes.bool,
